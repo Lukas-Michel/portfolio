ba356f1d80236881b84e02fafe1da39519038117
Lukas Michel <stubbe@Mac-Mini-2.fritz.box>
2025-05-16 19:08:01 +0200

    tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,4 +10,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [tailwind()],
 });
