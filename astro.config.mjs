3ecde6c631104a3f8d6c957d50c049cec943477d
Lukas Michel <stubbe@Mac-Mini-2.fritz.box>
2025-05-16 18:40:38 +0200

    Github Action for Deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://lukas-michel.github.io",
+  base: "portfolio",
   vite: {
     plugins: [tailwindcss()],
   },
